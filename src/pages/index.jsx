024c6f327be1c4234757a12ca6488b2169ac8595
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-06-11 17:18:14 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,11 @@
 import data from '../projects.json'
 import { useRouter } from 'next/router'
-import Divider from '@/components/atoms/Divider'
+import Section from '@/components/loco/Section'
 import OutlineText from '@/components/Animation/OutlineText'
 import TrackingText from '@/components/Animation/TextTracking'
 import LargeTextWithImages from '@/components/atoms/LargeTextWithImages'
 import TextRevealAnimation from '@/components/Animation/TextRevealAnimation'
 
-import BackToTop from '@/components/atoms/BackToTop'
 
 const Home = () => {
   const router = useRouter()
@@ -18,90 +17,60 @@ const Home = () => {
     router.push('/about')
   }
   return (
+    <div className="main">
 
-    <>
+      <Section>
 
-      <div data-scroll-container>
-        <div className="main">
+        <TextRevealAnimation
+          classNames='lg:pt-[10rem] pt-[5rem]'
+          lines={[
+            "Hello! I'm",
+            'Sahej Tuli,',
+            'a San Francisco',
+            'based front-end',
+            'web developer.']} />
 
-          <section data-scroll-section>
+        <section className='lg:pt-[300px] pt-[5rem]  lg:w-[50%] ml-auto pb-[5rem]'>
+          <p className='lg:text-lg text-smd font-medium'>        I am a 21-year-old student currently pursuing a Bachelor's degree in Computer Science at San Francisco State University.
+            <br /><br /> My passion lies in creating stunning websites that leave a lasting impression.  With a keen eye for design and a focus on delivering exceptional user experiences,  I strive to craft digital masterpieces that combine aesthetics and functionality.   <br /><br />
+            As I refine my skills in coding languages, user experience design, and front-end development, I'm actively on the lookout for exciting opportunities to apply my knowledge and contribute to real-world projects. <br /> </p>
 
-            <TextRevealAnimation
-              classNames='lg:mt-[10rem] mt-[5rem]'
-              lines={[
-                "Hello! I'm",
-                'Sahej Tuli,',
-                'a San Francisco',
-                'based front-end',
-                'web developer.']} />
+          <p className='lg:text-lg text-smd pt-[3rem] font-medium'>Scroll on, brave scroller!</p>
 
+          <TrackingText handleClick={handleSeeAboutMe} text="Learn more about me →" className='text-lg text-gray mt-5 cursor-pointer' />
 
-            <section className='lg:mt-[300px] mt-[5rem]  lg:w-[50%] ml-auto mb-[5rem]'>
-              <p className='lg:text-lg text-smd font-medium '>        I am a 21-year-old student currently pursuing a Bachelor's degree in Computer Science at San Francisco State University.
-                <br /><br /> My passion lies in creating stunning websites that leave a lasting impression.  With a keen eye for design and a focus on delivering exceptional user experiences,  I strive to craft digital masterpieces that combine aesthetics and functionality.   <br /><br />
-                As I refine my skills in coding languages, user experience design, and front-end development, I'm actively on the lookout for exciting opportunities to apply my knowledge and contribute to real-world projects. <br /> </p>
+        </section>
 
-              <p className='lg:text-lg text-smd mt-[3rem] font-medium'>Scroll on, brave scroller!</p>
+      </Section>
 
+      <Section>
 
-              <TrackingText handleClick={handleSeeAboutMe} text="Learn more about me →" className='text-lg text-gray mt-5 cursor-pointer' />
+        {[data[0], data[1]].map((project) => (
+          <LargeTextWithImages
+            href={project.slug}
+            heading={project.name}
+            second_image_url={project.gallary[1]}
+            first_image_url={project.gallary[0]}
+            discription={project.overview} />
+        ))}
 
-            </section>
-
-
-          </section>
-
-          <section data-scroll-section>
-
-            {[data[0], data[1]].map((project) => (
-              <LargeTextWithImages
-                href={project.slug}
-                heading={project.name}
-                second_image_url={project.gallary[1]}
-                first_image_url={project.gallary[0]}
-                discription={project.overview} />
-            ))}
+        <TrackingText handleClick={handleSeeAboutMe} text="See all projects →" className='text-lg text-gray mt-5 cursor-pointer' />
 
 
-          </section>
+      </Section>
 
 
-          <section data-scroll-section>
-
-            <h1 className=' 4xl:text-h2-4xl 3xl:text-h2-3xl 2xl:text-h2-2xl xl:text-h2-xl lg:text-h2-lg md:text-h2-md text-h2-sm !font-extrabold '>To get in touch,  </h1>
-            <OutlineText onClick={() => { window.location.href = "mailto:hello@sahejtuli.com" }} classNames=' 4xl:text-h2-4xl 3xl:text-h2-3xl 2xl:text-h2-2xl xl:text-h2-xl lg:text-h2-lg md:text-h2-md text-h2-sm !font-extrabold cursor-pointer' text='Email me!' />
-            <div className='flex gap-5 px-2 mt-5'>
-              <TrackingText handleClick={() => { window.open("https://linkedin.com/in/sahejtuli") }} text="LinkedIn" className='text-lg text-gray cursor-pointer' />
-              <TrackingText handleClick={() => { window.open("https://github.com/sahejt") }} text="GitHub" className='text-lg text-gray cursor-pointer' />
-            </div>
-
-          </section>
-
-
-
+      <Section>
+        <h1 className='4xl:text-h2-4xl 3xl:text-h2-3xl 2xl:text-h2-2xl xl:text-h2-xl lg:text-h2-lg md:text-h2-md text-h2-sm !font-extrabold '>To get in touch,</h1>
+        <OutlineText onClick={() => { window.location.href = "mailto:hello@sahejtuli.com" }} classNames='4xl:text-h2-4xl 3xl:text-h2-3xl 2xl:text-h2-2xl xl:text-h2-xl lg:text-h2-lg md:text-h2-md text-h2-sm !font-extrabold cursor-pointer' text='Email me!' />
+        <div className='flex gap-5 px-2 mt-5'>
+          <TrackingText handleClick={() => { window.open("https://linkedin.com/in/sahejtuli") }} text="LinkedIn" className='text-lg text-gray cursor-pointer' />
+          <TrackingText handleClick={() => { window.open("https://github.com/sahejt") }} text="GitHub" className='text-lg text-gray cursor-pointer' />
         </div>
-
-      </div>
-
-    </>
-
-
-
+      </Section>
+    </div>
   )
 }
 
 export default Home
-
-const Footer = () => {
-  return (
-    <div data-scroll-container className='mt-[5rem] bg-red-500'>
-      <h1 className=' 4xl:text-h2-4xl 3xl:text-h2-3xl 2xl:text-h2-2xl xl:text-h2-xl lg:text-h2-lg md:text-h2-md text-h2-sm !font-extrabold '>To get in touch,  </h1>
-      <OutlineText onClick={() => { window.location.href = "mailto:hello@sahejtuli.com" }} classNames=' 4xl:text-h2-4xl 3xl:text-h2-3xl 2xl:text-h2-2xl xl:text-h2-xl lg:text-h2-lg md:text-h2-md text-h2-sm !font-extrabold cursor-pointer' text='Email me!' />
-      <div className='flex gap-5 px-2 mt-5'>
-        <TrackingText handleClick={() => { window.open("https://linkedin.com/in/sahejtuli") }} text="LinkedIn" className='text-lg text-gray cursor-pointer' />
-        <TrackingText handleClick={() => { window.open("https://github.com/sahejt") }} text="GitHub" className='text-lg text-gray cursor-pointer' />
-      </div>
-    </div>
-  )
-}
 
